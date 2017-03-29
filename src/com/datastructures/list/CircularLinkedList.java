aa70ab4dde6456a519d33fafb1df6a125e4653ab
Marcos Hass Wakamatsu <marcoshass@gmail.com>
2017-03-28 22:46:07 -0300

    commit

--- src/com/datastructures/list/CircularLinkedList.java
+++ src/com/datastructures/list/CircularLinkedList.java
@@ -107,6 +107,105 @@ public class CircularLinkedList {
 		length++;
 	}
 	
-	// Deleting the last node in a circular list ...
+	/**
+	 * Returns data at the head of list. 
+	 */
+	public int peek() { // O(1)
+		// empty list
+		if (head == null) {
+			return 0;
+		} else {
+			return head.getData();
+		}
+	}
+	
+	/**
+	 * Returns data at tail of list. 
+	 */
+	public int tailPeek() { // O(n)
+		if (head == null){
+			// empty list
+			return 0;
+		} else if (head.getNext() == head) {
+			// one node list
+			return head.getData();
+		} else {
+			// two or more nodes
+			CLLNode p = head;
+			while (p.getNext() != head) {
+				p = p.getNext();
+			}
+			return p.getData(); 
+		}
+	}
 
+	/**
+	 * Returns and removes data from head of list. 
+	 */
+	public int removeFromHead() { // O(n)
+		
+		int data = 0;
+		if (head == null) {
+			return 0;
+		} else if (head.getNext() == head) {
+			// one node list
+			data = head.getData();
+			head.setNext(null);
+			head = null;
+			length--;
+		} else {
+			// two or more nodes
+			CLLNode p = head;
+			while (p.getNext() != head) {
+				p = p.getNext();
+			}
+			data = p.getData();
+
+			// adjust pointers
+			p.setNext(head.getNext());
+			head.setNext(null);
+			head = p;
+			
+			length--;
+		}
+		
+		return data;
+	}
+	
+	/**
+	 * Returns and removes data from tail of list.
+	 */
+	public int removeFromTail() { // O(n)
+		int data = 0;
+		
+		// empty list
+		if (head == null) {
+			return 0;
+		} else if (head.getNext() == head) {
+			// one node list
+			data = head.getData();
+			head.setNext(null);
+			head = null;
+			length--;
+		} else {
+			// two or more nodes
+			CLLNode p = head;
+			CLLNode p1 = p.getNext();
+
+			while (p1.getNext() != head) {
+				p1 = p1.getNext();
+				p = p.getNext();
+			}
+
+			// adjust pointers
+			p.setNext(p1.getNext());
+			p1.setNext(null);
+			length--;
+		}
+		
+		return data;
+	}
+
+	// public boolean contains(int data) ...
+	
 }
