08de8a6e03bfb9dfc3da53f22db36d2d0bb83aea
Marcos Hass Wakamatsu <marcoshass@gmail.com>
2017-04-12 12:39:39 -0300

    commit

--- src/com/datastructures/list/UnrolledLinkedList.java
+++ src/com/datastructures/list/UnrolledLinkedList.java
@@ -1,74 +1,105 @@
 package com.datastructures.list;
 
-public class UnrolledLinkedList {
+import java.io.Serializable;
+import java.util.AbstractList;
+import java.util.List;
+
+//ref https://github.com/l-tamas/Unrolled-linked-list/blob/master/src/org/megatherion/util/collections/UnrolledLinkedList.java
+
+public class UnrolledLinkedList<E> extends AbstractList<E> implements List<E>, Serializable {
 
 	/**
 	 * The maximum number of elements that can be stored in a single node.
 	 */
 	private int nodeCapacity;
-
+	
 	/**
-	 * The current size of this list.
+	 * The current size of this list.	
 	 */
 	private int size = 0;
-
+	
 	/**
 	 * The first node of this list.
 	 */
-	private ListNode firstNode;
-
+	private Node firstNode;
+	
 	/**
 	 * The last node of this list.
 	 */
-	private ListNode lastNode;
+	private Node lastNode;
 
 	/**
-	 * Constructs an empty list with the specified capacity.
+	 * Constructs an empty list with the specified
+	 * {@link UnrolledLinkedList#nodeCapacity nodeCapacity}. For performance
+	 * reasons <tt>nodeCapacity</tt> must be greater tha or equal to 8.
+	 * 
+	 * @param nodeCapacity The maximum number of elements 
+	 * 		  that can be stored in a single node. 
+	 * @throws IllegalArgumentException if <tt>nodeCapacity</tt> is less than 8
 	 */
 	public UnrolledLinkedList(int nodeCapacity) throws IllegalArgumentException {
 		if (nodeCapacity < 8) {
 			throw new IllegalArgumentException("nodeCapacity < 8");
 		}
-		this.nodeCapacity = nodeCapacity;
-		firstNode = new ListNode();
-		lastNode = new ListNode();
-	}
 
+		this.nodeCapacity = nodeCapacity;
+		firstNode = new Node();
+		lastNode = firstNode;
+	}
+	
+	/**
+	 * Constructs an empty list with
+	 * {@link UnrolledLinkedList#nodeCapacity nodeCapacity} of 16.
+	 */
 	public UnrolledLinkedList() {
 		this(16);
 	}
-
+	
+	/**
+	 * Returns the number of elements in this list.
+	 */
 	public int size() {
 		return size;
 	}
-
+	
+	/**
+	 * Returns <tt>true</tt> if this list contains no elements.
+	 */
 	public boolean isEmpty() {
-		return size == 0;
+		return (size == 0);
 	}
+	
+	
 
-	public boolean contains(Object o) {
-		return indexOf(o) != -1;
-	}
-
-	public Iterator<E> iterator() {
-		return new ULLIterator(firstNode, 0, 0);
-	}
-
-	public boolean add(E e) {
-		insertIntoNode(lastNode, lastNode.numElements, e);
-	}
-
-	private static final long serialVersionUID = -674052309103045211L;
-
-	private class ListNode {
-		ListNode next;
-		ListNode previous;
+	private class Node {
+		
+		/**
+		 * The next node.
+		 */
+		Node next;
+		
+		/**
+		 * The previous node.
+		 */
+		Node previous;
+		
+		/**
+		 * The number of elements store in this node.
+		 */
 		int numElements = 0;
+		
+		/**
+		 * The array in which the elements are stored.
+		 */
 		Object[] elements;
-
-		ListNode() {
+		
+		/**
+		 * Constructs a new node based on the unrolled linked list capacity.
+		 */
+		Node() {
 			elements = new Object[nodeCapacity];
 		}
+		
 	}
-
+	
 }
