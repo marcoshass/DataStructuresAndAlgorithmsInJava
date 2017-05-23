c5c763d087329c33d509da09bbd457e3d10d47fb
Marcos Hass Wakamatsu <marcoshass@gmail.com>
2017-05-23 07:40:58 -0300

    commit

--- src/com/algorithms/recursion/StringsOfNBits.java
+++ src/com/algorithms/recursion/StringsOfNBits.java
@@ -4,23 +4,24 @@ import java.util.Arrays;
 
 public class StringsOfNBits {
 
-	private int[] A = {-1, -1};
+	private int[] A = new int[2];
 
 	public static void main(String[] args) {
-		new StringsOfNBits().binary(0);
+		StringsOfNBits s = new StringsOfNBits();
+		s.binary(s.A.length - 1);
 	}
 
 	void binary(int n) {
-		if (n == A.length) {
-			System.out.print(Arrays.toString(A));
+		if (n < 0) {
+			System.out.println(Arrays.toString(A));
 			return;
 		}
-
+		
 		A[n] = 0;
-		binary(n + 1);
+		binary(n - 1);
 		
 		A[n] = 1;
-		binary(n + 1);
+		binary(n - 1);
 	}
 
 }
