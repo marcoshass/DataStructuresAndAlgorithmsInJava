6458beadad5cbb73b0eb8d202649ee3d443c7191
Marcos Hass Wakamatsu <marcoshass@gmail.com>
2017-07-08 20:45:46 -0300

    commit

--- src/com/datastructures/list/problems/LinkedListReverseKNodes.java
+++ src/com/datastructures/list/problems/LinkedListReverseKNodes.java
@@ -10,19 +10,55 @@ public class LinkedListReverseKNodes {
 		ListNode node3 = new ListNode(3);
 		ListNode node4 = new ListNode(4);
 		ListNode node5 = new ListNode(5);
+		ListNode node6 = new ListNode(6);
+		ListNode node7 = new ListNode(7);
+		ListNode node8 = new ListNode(8);
+		ListNode node9 = new ListNode(9);
+		ListNode node10 = new ListNode(10);
 		
 		node1.setNext(node2);
 		node2.setNext(node3);
 		node3.setNext(node4);
 		node4.setNext(node5);
+		node5.setNext(node6);
+		node6.setNext(node7);
+		node7.setNext(node8);
+		node8.setNext(node9);
+		node9.setNext(node10);
 		
 		LinkedList l = new LinkedList();
 		
 		l.setHead(node1);
 		System.out.println(l);
 		
-		l.setHead(reverseKNodes(node1, 2));
+		l.setHead(reverseKNodesRecursive(node1, 2));
 		System.out.println(l);
+	}
+	
+	// time complexity: O(n)
+	// space complexity: O(n)
+	static ListNode reverseKNodesRecursive(ListNode head, int k) {
+		ListNode A = head;
+		ListNode B = A;
+		ListNode ret = null;
+		
+		for (int i = 1; i < k; i++) {
+			if (B.getNext() == null) // insuficient number of items to satisfy k
+				return A;
+			
+			B = B.getNext();
+		}
+		
+		if (B.getNext() != null) {
+			ListNode next = B.getNext();
+			B.setNext(null); // break the list
+			ret = reverseKNodesRecursive(next, k);
+		}
+		
+		// reverse A and B
+		reverse(A);
+		A.setNext(ret);
+		return B;
 	}
 	
 	// time complexity: O(n)
