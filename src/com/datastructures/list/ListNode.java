48ee890e1da46a6c393b9acfdf2a9432fa5aeb28
Marcos Hass Wakamatsu <marcoshass@gmail.com>
2017-07-12 22:22:45 -0300

    commit

--- src/com/datastructures/list/ListNode.java
+++ src/com/datastructures/list/ListNode.java
@@ -3,7 +3,7 @@ package com.datastructures.list;
 public class ListNode {
 
 	private int data;
-	
+
 	public ListNode(int data) {
 		this.data = data;
 	}
@@ -24,6 +24,19 @@ public class ListNode {
 
 	public void setNext(ListNode next) {
 		this.next = next;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (o != null && o instanceof ListNode) {
+			return this.getData() == ((ListNode) o).getData();
+		}
+		return false;
+	}
+	
+	@Override
+	public int hashCode() {
+		return data;
 	}
 
 }
