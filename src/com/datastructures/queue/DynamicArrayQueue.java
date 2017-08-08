50a61d8aebbe5414475f44f0675fd541d526413a
Marcos Hass Wakamatsu <marcoshass@gmail.com>
2017-08-08 06:19:48 -0300

    commit

--- src/com/datastructures/queue/DynamicArrayQueue.java
+++ src/com/datastructures/queue/DynamicArrayQueue.java
@@ -9,12 +9,12 @@ public class DynamicArrayQueue implements Queue {
 	private int front;
 
 	private int rear;
-	
+
 	public DynamicArrayQueue(int capacity) {
 		this.queue = new int[capacity];
 		reset();
 	}
-	
+
 	@Override
 	public void enqueue(int value) {
 		if (front == -1 && rear == 0) {
@@ -29,47 +29,84 @@ public class DynamicArrayQueue implements Queue {
 		}
 		size++;
 	}
-	
+
 	private int[] expand() {
-		int[] expandedArray = new int[queue.length*2];
-		
+		int[] expandedArray = new int[queue.length * 2];
+
 		for (int i = 0; i < getSize(); i++) {
 			expandedArray[i] = dequeue();
 		}
-		
+
+		// adjust pointers
+		front = 0;
+		rear = getSize();
+
 		return expandedArray;
+	}
+
+	private int[] shrink() {
+		int[] shrinkedArray = new int[queue.length / 2];
+
+		for (int i = 0; i < getSize(); i++) {
+			shrinkedArray[i] = dequeue();
+		}
+
+		// adjust pointers
+		front = 0;
+		rear = getSize()-1;		
+		
+		return shrinkedArray;
 	}
 
 	@Override
 	public int dequeue() {
+		int dequeueValue = 0;
 
-		return 0;
+		// cannot dequeue is queue is empty
+		if (isReseted()) {
+			throw new EmptyQueueException();
+		}
+
+		dequeueValue = queue[front];
+		if (front == rear) {
+			reset();
+		} else {
+			front = (front + 1) % queue.length;
+			size--;
+		}
+
+		// shrink when only 1/4 of queue is full
+		if (size <= queue.length / 4) {
+			queue = shrink();
+		}
+
+		return dequeueValue;
 	}
 
 	@Override
 	public int getSize() {
 		return size;
 	}
-	
+
 	public int getRawSize() {
 		return queue.length;
 	}
-	
+
 	private void reset() {
 		size = 0;
 		front = -1;
 		rear = 0;
 	}
-	
+
 	private boolean isReseted() {
 		return size == 0 && front == -1 && rear == 0;
 	}
 
 	// tests
-	
+
 	private static final int CAPACITY = 8;
 
-	private static Queue createQueue() {
+	private static Queue createFullQueue() {
 		Queue queue = new DynamicArrayQueue(CAPACITY);
 		for (int i = 1; i <= CAPACITY; i++) {
 			queue.enqueue(i);
@@ -77,18 +114,30 @@ public class DynamicArrayQueue implements Queue {
 		return queue;
 	}
 	
-	private static void testExpandQueue() {
-		Queue queue = createQueue();
-		assert(queue.getSize() == CAPACITY);
+	private static void testShrinkQueue() {
+		Queue queue = createFullQueue();
+		assert (queue.getSize() == CAPACITY);
+
+		while (queue.getSize() > queue.getRawSize()/4) {
+			queue.dequeue();
+		}
 		
-		queue.enqueue(9);
-		assert(queue.getRawSize() == CAPACITY*2);
+		assert (queue.getRawSize() == queue.getSize() * 2);
 	}
-	
+
+	private static void testExpandQueue() {
+		Queue queue = createFullQueue();
+		assert (queue.getSize() == CAPACITY);
+
+		queue.enqueue(0);
+		assert (queue.getRawSize() == CAPACITY * 2);
+	}
+
 	private static void unitTests() {
 		testExpandQueue();
+		testShrinkQueue();
 	}
-	
+
 	public static void main(String[] args) {
 		unitTests();
 		System.out.println("You passed all tests!");
