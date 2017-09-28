018f74676cb42051d6654c29a770b95a946d1b6e
Marcos Hass Wakamatsu <marcoshass@gmail.com>
2017-09-28 17:23:16 -0300

    commit

---
+++ src/com/practive/epi/ComputerXPowerY.java
@@ -0,0 +1,26 @@
+package com.practive.epi;
+
+public class ComputerXPowerY {
+
+	public static void main(String[] args) {
+		System.out.println(power(12, 3));
+	}
+
+	public static double power(double x, int y) {
+		double result = 1.0;
+		long power = y;
+		if (y < 0) {
+			power = -power;
+			x = 1.0 / x;
+		}
+		while (power != 0) {
+//			if ((power & 1) != 0) {
+//				result *= x;
+//			}
+			x *= x;
+			power >>>= 1;
+		}
+		return result;
+	}
+
+}
