ee3811c7d9308ab186558e7dc40e5084abb2191b
Marcos Hass <marcos@mirasoft.com.br>
2018-03-20 18:16:57 -0300

    commit

--- src/com/practice/leetcode/LongestSubstring.java
+++ src/com/practice/leetcode/LongestSubstring.java
@@ -5,7 +5,7 @@ import java.util.Hashtable;
 public class LongestSubstring {
 
 	public static void main(String[] args) {
-		System.out.println("Length of the longest: " + lengthOfLongestSubstring("c"));
+		System.out.println("Length of the longest: " + lengthOfLongestSubstring("dvdf"));
 	}
 
 	static int lengthOfLongestSubstring(String s) {
@@ -13,17 +13,25 @@ public class LongestSubstring {
 		Hashtable<Character, Integer> letters = new Hashtable<>();
 
 		for (int i = 0; i < s.length(); i++) {
-			if (letters.containsKey(s.charAt(i)) || i == s.length() - 1) {
-				longest = acum.length() > longest.length() ? acum : longest;
+			if (!letters.containsKey(s.charAt(i))) {
+				letters.put(s.charAt(i), i);
+				acum += s.charAt(i);
+				if (i == s.length() - 1) {
+					longest = getMaxString(acum, longest);
+				}
+
+			} else {
+				longest = getMaxString(acum, longest);
 				acum = String.valueOf(s.charAt(i));
 				letters.clear();
 				letters.put(s.charAt(i), i);
-			} else {
-				letters.put(s.charAt(i), i);
-				acum += s.charAt(i);
 			}
 		}
 		return longest.length();
+	}
+
+	static String getMaxString(String s1, String s2) {
+		return s1.length() > s2.length() ? s1 : s2;
 	}
 
 }
