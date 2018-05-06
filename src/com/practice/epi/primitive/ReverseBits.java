d55a69fda812c4c8c4b491be6c0dc765062635f7
marcos <marcoshass@gmail.com>
2018-05-05 21:24:18 -0300

    commit

--- src/com/practice/epi/primitive/ReverseBits.java
+++ src/com/practice/epi/primitive/ReverseBits.java
@@ -2,45 +2,42 @@ package com.practice.epi.primitive;
 
 public class ReverseBits {
 
+	static long[] precomputedReverse = new long[65536];
+
+	static {
+		for (int i = 0; i < precomputedReverse.length; i++) {
+			precomputedReverse[i] = reverseBits(i);
+		}
+	}
+	
 	public static void main(String[] args) {
-		long x = 0b10111111;
-		//System.out.printf("original:%s, reverse:%s", Long.toBinaryString(x), Long.toBinaryString(reverseBits(x)));
-		//System.out.printf("original:%s, reverse:%s", Long.toBinaryString(x), Long.toBinaryString(reverseBits1(x)));
-		precomputedReverse();
+		long x = 0b10010011l;
+		System.out.printf("original:%s, reverse=%s", Long.toBinaryString(x), Long.toBinaryString(reverseBits(x)));
+		System.out.printf("\noriginal:%s, reverse=%s <--table", Long.toBinaryString(x), Long.toBinaryString(reverseBitsTable(x)));
 	}
 	
 	// 1:: O(nbits)
-	public static long reverseBits(long x) {
+	static long reverseBits(long x) {
 		long y = 0;
 		
 		while (x != 0) {
 			y <<= 1;
-			y |= (x & 1);
+			y |= x & 1;
 			x >>>= 1;
 		}
 		
 		return y;
 	}
 	
-	// 2:: table based
-	public static long reverseBits1(long x) {
-		final long BIT_MASK = 0b00000011;
-		final int WORD_LENGTH = 2;
-		return
-				precomputedReverse()[(int)(x & BIT_MASK)] |
-				x & (BIT_MASK << WORD_LENGTH) |
-				x & (BIT_MASK << 2 * WORD_LENGTH |
-				BIT_MASK << 3 * WORD_LENGTH; 
+	// 2:: Table-based version
+	static long reverseBitsTable(long x) {
+		final int MASK_SIZE = 16;
+		final int BIT_MASK = 0xFFFF;
+		
+		return precomputedReverse[(int) (x & BIT_MASK)] << (3 * MASK_SIZE)
+				| precomputedReverse[(int) ((x >>> MASK_SIZE) & BIT_MASK)] << (2 * MASK_SIZE)
+				| precomputedReverse[(int) ((x >>> (2 * MASK_SIZE)) & BIT_MASK)] << MASK_SIZE
+				| precomputedReverse[(int) ((x >>> (3 * MASK_SIZE)) & BIT_MASK)];
 	}
-
-	static final int CACHE_SIZE = 256;
 	
-	public static int[] precomputedReverse() {
-		int[] cache = new int[CACHE_SIZE];
-		for (int i = 0; i < CACHE_SIZE; i++) {
-			cache[i] = (int)reverseBits(i);
-		}
-		return cache;
-	}
-
 }
