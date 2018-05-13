4f4b507f40571501b77da63db66043bdd65aa0db
marcos <marcoshass@gmail.com>
2018-05-13 11:33:47 -0300

    commit

--- src/com/practice/epi/strings/InterconvertStringInteger.java
+++ src/com/practice/epi/strings/InterconvertStringInteger.java
@@ -3,9 +3,9 @@ package com.practice.epi.strings;
 public class InterconvertStringInteger {
 
 	public static void main(String[] args) {
-		int x = 2048;
-		String s = "2048";
-
+		int x = -2048;
+		String s = String.valueOf(x);
+		
 		System.out.printf("original:%d, toString:\"%s\"", x, intToString(x));
 		System.out.printf("\noriginal:\"%s\" toInt:%d", s, stringToInt(s));
 	}
@@ -14,7 +14,7 @@ public class InterconvertStringInteger {
 		final boolean isNegative = x < 0;
 		StringBuilder s = new StringBuilder(x == 0 ? "0" : "");
 		while (x != 0) {
-			s.append(Math.abs(x % 10));	
+			s.append(Math.abs(x % 10));
 			x /= 10;
 		}
 		return s.append(isNegative ? "-" : "").reverse().toString();
@@ -24,17 +24,7 @@ public class InterconvertStringInteger {
 		final boolean isNegative = s.startsWith("-");
 		int result = 0;
 		for (int i = isNegative ? 1 : 0; i < s.length(); i++) {
-			result = (result * 10) + Integer.valueOf(s.substring(i,i+1));
-		}
-		return isNegative ? result * -1 : result;
-	}
-
-	// brute force:: multiply by powers of 10
-	public static int stringToInt1(String s) {
-		final boolean isNegative = s.startsWith("-");
-		int result = 0;
-		for (int exp = 0, i = s.toCharArray().length - 1; i >= (isNegative ? 1 : 0); i--, exp++) {
-			result += Integer.valueOf(s.substring(i,i+1)) * Math.pow(10, exp);
+			result = (result * 10) + s.charAt(i) - '0';
 		}
 		return isNegative ? result * -1 : result;
 	}
