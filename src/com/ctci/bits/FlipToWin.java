a038c5d3fb2bf8b604198e0edf2448f7df794510
marcos <marcoshass@gmail.com>
2018-05-16 08:53:01 -0300

    commit

--- src/com/ctci/bits/FlipToWin.java
+++ src/com/ctci/bits/FlipToWin.java
@@ -10,9 +10,7 @@ public class FlipToWin {
 	
 	static int flipBits(int a) {
 		while (a != 0) {
-			if ((a & 3) == 2) {
-				System.out.println(a);
-			}
+			System.out.printf("\ndecimal: %d %s: %b", a, Long.toBinaryString(a), (a & 3) == 1);
 			a >>>= 1;
 		}
 		return a;
