aac36f4e8a56f227313cd4484a78fc7e840281f5
marcos <marcoshass@gmail.com>
2018-05-16 08:44:52 -0300

    commit

--- src/com/ctci/bits/FlipToWin.java
+++ src/com/ctci/bits/FlipToWin.java
@@ -3,8 +3,19 @@ package com.ctci.bits;
 public class FlipToWin {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int x = 0b11011101111;
+		flipBits(x);
+		//System.out.printf("\n%d", flipBits(x));
+	}
+	
+	static int flipBits(int a) {
+		while (a != 0) {
+			if ((a & 3) == 2) {
+				System.out.println(a);
+			}
+			a >>>= 1;
+		}
+		return a;
 	}
 
 }
