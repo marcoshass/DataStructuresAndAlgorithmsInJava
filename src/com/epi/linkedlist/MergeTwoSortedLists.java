c711d09fc24c18e7bfa2ce6d93fdf6d1b339ce1c
marcos <marcos@mini-de-marcos>
2018-05-24 08:14:05 -0300

    commit

--- src/com/epi/linkedlist/MergeTwoSortedLists.java
+++ src/com/epi/linkedlist/MergeTwoSortedLists.java
@@ -29,27 +29,3 @@ public class MergeTwoSortedLists {
 	}
 }
 
-class ListNode<T> {
-	public T data;
-	public ListNode<T> next;
-
-	public ListNode(T data) {
-		this.data = data;
-	}
-
-	public ListNode(T data, ListNode<T> next) {
-		this(data);
-		this.next = next;
-	}
-	
-	@Override
-	public String toString() {
-		StringBuilder sb = new StringBuilder();
-		for (ListNode<T> p = this; p != null; p = p.next) {
-			sb.append(p.data);
-			sb.append("->");
-		}
-		sb.append("null");
-		return sb.toString();
-	}
-}
